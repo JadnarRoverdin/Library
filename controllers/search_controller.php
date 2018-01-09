d6be46ce2f418be065aafe5bad67d9e6cc02573b
JadnarRoverdin <jadnarroverdin@gmail.com>
2018-01-08 20:15:53 -0500

    seperated results view

--- controllers/search_controller.php
+++ controllers/search_controller.php
@@ -21,24 +21,7 @@ Class SearchController
         $booklist = Book::searchBySeries($string)[1];
         break;
     }
-    echo "Searching $searchBy's for '$string' yielded ".sizeof($booklist)." results.<hr><hr>";
-
-    echo "<div class='resultsContainer'>";
-    for($i = 0; $i < sizeof($booklist); $i++)
-    {
-      echo "<div class='result'>
-              Title: ".$booklist[$i]->title."<br>
-              ISBN: ".$booklist[$i]->ISBN."<br>
-              Author(s): ";
-              foreach($booklist[$i]->authors as $a)
-              {
-                echo $a->firstName." ".$a->middleName." ".$a->lastName.",";
-              }
-              if($booklist[$i]->bookNumber > 0)
-                echo "<br>Book ".$booklist[$i]->bookNumber." of ".$booklist[$i]->series->name;
-            echo "</div><hr>";
-    }
-    echo "</div>";
+  require_once('views/search/searchResults.php');
   }
 }
 
---
+++ views/search/searchResults.php
@@ -0,0 +1,20 @@
+<?php
+echo "Searching $searchBy's for '$string' yielded ".sizeof($booklist)." results.<hr><hr>";
+
+echo "<div class='resultsContainer'>";
+for($i = 0; $i < sizeof($booklist); $i++)
+{
+  echo "<div class='result'>
+          Title: ".$booklist[$i]->title."<br>
+          ISBN: ".$booklist[$i]->ISBN."<br>
+          Author(s): ";
+          foreach($booklist[$i]->authors as $a)
+          {
+            echo $a->firstName." ".$a->middleName." ".$a->lastName.",";
+          }
+          if($booklist[$i]->bookNumber > 0)
+            echo "<br>Book ".$booklist[$i]->bookNumber." of ".$booklist[$i]->series->name;
+        echo "</div><hr>";
+}
+echo "</div>";
+?>
